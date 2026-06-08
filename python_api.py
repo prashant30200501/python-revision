0c196ebcf324852a71303afe32b1a4051bdbe6e4
prashant30200501 <jangidprashant28@gmail.com>
2026-06-08 12:31:15 +0530

    python to json

--- python_api.py
+++ python_api.py
@@ -1,10 +1,23 @@
-import requests 
+"""import requests 
 data ={
     "name" : "piyush",
-    "age" : [21]
+    "age" : 21
 }
 
 response = requests.post( "https://jsonplaceholder.typicode.com/users", json=data)
 
 
 print(response.json())
+print(response.status_code)
+"""
+
+import json
+
+user = {
+    "name":"piyush",
+    "age": 20
+}
+
+json_data = json.dumps(user)
+
+print(json_data)
