e774f692cb8551fddb0b99c502db18e4d6c83e44
prashant30200501 <jangidprashant28@gmail.com>
2026-06-10 15:37:40 +0530

    add one more post

--- api_main.py
+++ api_main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+"""from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -6,4 +6,16 @@ app = FastAPI()
 def create_user(name: str):
     return {
         "message": f"Welcome {name}"
+    }"""
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/car")
+def get_car(name :str , year : int):
+    return{
+        "message": f"car is {name} year is {year}"
+        
     }
